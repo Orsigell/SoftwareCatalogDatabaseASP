f24f84a6124e3bc0a3ce74402c798ce864fbbe90
Илья <ilyant2000@gmail.com>
2023-05-10 11:55:04 +0300

    Добавление категорий и скриншотов лб2

--- Controllers/AddSoftwareController.cs
+++ Controllers/AddSoftwareController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Packaging;
 using SoftwareCatalogDatabaseASP.Models;
 
 namespace SoftwareCatalogDatabaseASP.Controllers
@@ -17,15 +18,89 @@ namespace SoftwareCatalogDatabaseASP.Controllers
             return View(await _context.Softwares.ToListAsync());
 		}
 		[HttpPost]
-		public async Task<IActionResult> Index(int softwareId)
+        public async Task<IActionResult> Index(int softwareId)
+        {
+            Software software = await _context.Softwares.Include(s => s.Categories).FirstOrDefaultAsync(s => s.Id == softwareId);
+            if (software == null)
+                return NotFound();
+
+            ViewBag.SoftwareId = softwareId;
+            ViewBag.Software = software;
+            return View("ChooseCategory", await _context.Categories.Distinct().ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChooseCategory(int softwareId, int[] selectedCategories)
+        {
+            Software software = await _context.Softwares.Include(s => s.Categories).FirstOrDefaultAsync(s => s.Id == softwareId);
+
+
+            if (software == null)
+                return NotFound();
+
+            foreach (var item in selectedCategories)
+            {
+                Categories categories = await _context.Categories.FindAsync(item);
+                if (categories == null)
+                    return NotFound();
+                if (software.Categories.FirstOrDefault(c => c.Name == categories.Name) == null)
+				{
+					Categories newCategorie = new Categories()
+					{
+						Name = categories.Name,
+						Software = software,
+						SoftwareId = softwareId
+					};
+					software.Categories.Add(newCategorie);
+				}
+            }
+
+            await _context.SaveChangesAsync();
+            return View("ChooseImages", await _context.Screens.ToListAsync());
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> ChooseImages(int softwareId)
+        {
+            Software software = await _context.Softwares.FindAsync(softwareId);
+
+            if (software == null)
+                return NotFound();
+
+            ViewBag.SoftwareId = softwareId;
+            ViewBag.SoftwareName = software.Name;
+
+            return View(new List<IFormFile>());
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> ChooseImages(int softwareId, List<IFormFile> screenshots)
 		{
-			Software software = await _context.Softwares.FindAsync(softwareId);
+			Software software = await _context.Softwares.Include(s => s.Screens).FirstOrDefaultAsync(s => s.Id == softwareId);
+
 			if (software == null)
 				return NotFound();
 
-			ViewBag.SoftwareId = softwareId;
-			ViewBag.Software = software;
-			return View("ChooseCategory", await _context.Categories.ToListAsync());
-		}
-	}
+			var newScreens = new List<Screens>();
+            foreach (var screenshot in screenshots)
+            {
+                if (screenshot.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await screenshot.CopyToAsync(stream);
+                    }
+                    newScreens.Add(new Screens { Screen = filePath, SoftwareId = softwareId });
+                }
+            }
+
+            software.Screens.AddRange(newScreens);
+            await _context.SaveChangesAsync();
+
+			return RedirectToAction("Index", "Softwares");
+        }
+    }
 }
